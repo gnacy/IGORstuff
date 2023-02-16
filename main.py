56d538c37c1daab42862d39d8fbefcfaec10754f
gnacy <124902365+gnacy@users.noreply.github.com>
2023-02-16 16:23:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+imie = "phillipe"
+nazwisko = "padek"
+
+print(imie + " " + nazwisko)
+#wpisuje imie i nazwisko
+
+print([imie + " " + nazwisko]*3) #3 razy
+
+print(f"siema siema o tej porze {imie} wypić może")#żul
+
+x = input("podaj swój wiek ")
+print(int(x) + 44)
+
+bond = 7
+print(str(bond).zfill(int(3)))
